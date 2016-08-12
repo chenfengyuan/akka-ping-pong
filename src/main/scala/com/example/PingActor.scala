027741c8144c472e9a04666be4cc018bacdbdde3
Fengyuan Chen <cfy1990@gmail.com>
2016-08-12 16:12:14 +0800

    minor changes

--- src/main/scala/com/example/PingActor.scala
+++ src/main/scala/com/example/PingActor.scala
@@ -3,24 +3,27 @@ package com.example
 import akka.actor.{Actor, ActorLogging, Props}
 
 class PingActor extends Actor with ActorLogging {
+
   import PingActor._
-  
+
   var counter = 0
   val pongActor = context.actorOf(PongActor.props, "pongActor")
-
+  var start = scala.compat.Platform.currentTime
+  val total = 1000 * 10000
   def receive = {
-  	case Initialize => 
-	    log.info("In PingActor - starting ping-pong")
-  	  pongActor ! PingMessage("ping")	
-  	case PongActor.PongMessage(text) =>
-  	  counter += 1
-  	  if (counter == 100000) {
-        log.info("In PingActor - received message: {}", text)
+    case Initialize =>
+      start = scala.compat.Platform.currentTime
+      log.info("In PingActor - starting ping-pong")
+      pongActor ! PingMessage("ping")
+    case PongActor.PongMessage(text) =>
+      counter += 1
+      if (counter == total) {
+        log.info(s"${total * 2} messages, ${scala.compat.Platform.currentTime - start}ms, ${total * 2.0 / (scala.compat.Platform.currentTime - start) * 1000} msg/s")
         context.system.shutdown()
       }
       else {
-        if (counter % 10000 == 0)
-          log.info("In PingActor - received message: {}", text)
+        if (counter % 100000 == 0)
+          log.info(s"$counter/$total")
         sender() ! PingMessage("ping")
       }
   }
@@ -28,6 +31,9 @@ class PingActor extends Actor with ActorLogging {
 
 object PingActor {
   val props = Props[PingActor]
+
   case object Initialize
+
   case class PingMessage(text: String)
+
 }
